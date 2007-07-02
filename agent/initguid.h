05f826129cdee4d99f5867b2673613cd819d0688
kzenner <kzenner@9460fa18-4d33-0410-9079-893bd613e7d8>
2007-07-02 09:43:55 +0000

    deleting cause new Agent is in repository
    git-svn-id: https://openmailarchiva.svn.sourceforge.net/svnroot/openmailarchiva/Server/trunk@46 9460fa18-4d33-0410-9079-893bd613e7d8

--- agent/initguid.h
+++
@@ -1,6 +0,0 @@
-#if !defined(INITGUID) || defined(USES_IID_IConverterSession)
- // {4e3a7680-b77a-11d0-9da5-00c04fd65685}
- DEFINE_GUID(CLSID_IConverterSession, 0x4e3a7680, 0xb77a, 0x11d0, 0x9d, 0xa5, 0x0, 0xc0, 0x4f, 0xd6, 0x56, 0x85);
- // {4b401570-b77b-11d0-9da5-00c04fd65685}
- DEFINE_GUID(IID_IConverterSession, 0x4b401570, 0xb77b, 0x11d0, 0x9d, 0xa5, 0x0, 0xc0, 0x4f, 0xd6, 0x56, 0x85);
- #endif // #if !defined(INITGUID) || defined(USES_IID_IConverterSession)
