5ae37576536716b406e1ff551e75436fe677e292
Kazuo KASHIMA <k4200@kazu.tv>
2013-04-18 00:48:24 +0900

    (refs #9) Mod Extractor to use POI for Office 2007+ files

--- src/com/stimulus/archiva/extraction/Extractor.java
+++ src/com/stimulus/archiva/extraction/Extractor.java
@@ -58,36 +58,39 @@ public class Extractor implements Serializable
 		handlers.put("rtf", rtf);
 		
 		TextExtractor poi = new POIExtractor();
-		TextExtractor ms2007 = new MS2007Extractor();
+
 		// word
 		handlers.put("application/msword",poi);
-		handlers.put("application/vnd.ms-word", ms2007);
-		handlers.put("application/vnd.msword", ms2007);
-		handlers.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document",ms2007);
-		handlers.put("application/vnd.openxmlformats-officedocument.wordprocessingml.template",ms2007);
+		handlers.put("application/vnd.ms-word", poi);
+		handlers.put("application/vnd.msword", poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document",poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.wordprocessingml.template",poi);
 		handlers.put("doc",poi);
-		handlers.put("docx",ms2007);
-		handlers.put("dotx",ms2007);
+		handlers.put("docx",poi);
+		handlers.put("dotx",poi);
 		
 		// excel
 		
 		handlers.put("application/excel", poi);
 		handlers.put("application/msexcel", poi);
-		handlers.put("application/vnd.ms-excel", ms2007);
-		handlers.put("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", ms2007);
-		handlers.put("application/vnd.openxmlformats-officedocument.spreadsheetml.template",ms2007);
+		handlers.put("application/vnd.ms-excel", poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.spreadsheetml.template",poi);
 		handlers.put("xls", poi);
-		handlers.put("xlsx",ms2007);
+		handlers.put("xlsx",poi);
 		
 		// powerpoint
 		
-		handlers.put("application/vnd.ms-powerpoint", ms2007);
+		handlers.put("application/vnd.ms-powerpoint", poi);
 		handlers.put("application/mspowerpoint", poi);
 		handlers.put("application/powerpoint", poi);
 		handlers.put("ppt", poi);
-		handlers.put("pptx",ms2007);
-		handlers.put("potx",ms2007);
-		handlers.put("sldx",ms2007);
+		handlers.put("pptx",poi);
+		handlers.put("potx",poi);
+		handlers.put("sldx",poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.presentationml.presentation", poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.presentationml.template", poi);
+		handlers.put("application/vnd.openxmlformats-officedocument.presentationml.slidshow", poi);
 		
 		// open office
 		
@@ -116,6 +119,7 @@ public class Extractor implements Serializable
 	     TextExtractor extractor;
 	     extractor = handlers.get(mimetype.toLowerCase(Locale.ENGLISH));
 	     if(extractor == null) {
+	    	 logger.debug("failed to extract text (mimetype not supported) {mimetype='"+mimetype+"'}");
 	     	//throw new ExtractionException("failed to extract text (mimetype not supported) {mimetype='"+mimetype+"'}",logger);
 	      return null;
 	     } else {
